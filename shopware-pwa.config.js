467736cd55d5c7f393a8ee506fca2dc73c6aa672
mkucmus <maciejkucmus@gmail.com>
2021-06-07 12:03:05 +0200

    init

---
+++ shopware-pwa.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  shopwareEndpoint: "https://pwa-demo-api.shopware.com/trunk",
+  shopwareAccessToken: "SWSC40-LJTNO6COUEN7CJMXKLA",
+  pwaHost: "https://pwa-demo-api.shopware.com/trunk/",
+};
